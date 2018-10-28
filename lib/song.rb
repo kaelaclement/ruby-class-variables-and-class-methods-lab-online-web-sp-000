cb4ad130badec3e7355c6022103da04e58560b80
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-28 22:18:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,8 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@genres << genre if !@@genres.include?(genre)
+    @@genres << genre
+    @@artists << artist
   end
 
 end
